d68f668493823c8d793b6bb9a4da07165bd5ebc2
sss <1623958071@qq.com>
2022-03-30 13:46:50 +0800

    毕设爬水果

--- 爬虫/爬水果热量/水果.py
+++ 爬虫/爬水果热量/水果.py
@@ -1,22 +1,83 @@
 import requests
-import  re
-from lxml import etree
-url = "https://www.boohee.com/food/group/4"
-head={
-"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39?page={page}".format(page=1),
-}
-# parmer={
-#     "page":"1"
-# }
+from  bs4  import BeautifulSoup
 
-get = requests.get(url=url,headers=head)
-print(get.text)
-#==============
-with open('./水果.html', 'w',encoding="utf-8") as f:
-    f.write(get.text)
-# /html/body/div[1]/div[2]/div[2]/div/div[2]/ul/li[1]/div[2]/h4/a
-selector = etree.HTML(get.text)#转到了xpath的文件下
-# html_data = re.findall('<li><a href="/shiwu/"></a>', get.text)
-# print(html_data)
-data = selector.xpath('/html/body/div[1]/div[2]')
-print(data)
+def get_web(str):
+    web = str.split('"')[1]
+    return web
+
+def Nutrition_name(dt):
+    Nutrition_list = []
+    for name in dt:
+        name = name.text
+        Nutrition_list.append(name)
+    return Nutrition_list[2:]
+def Nutrition_value(dd):
+    Nutrition_list = []
+    for name in dd:
+        name = name.text
+        Nutrition_list.append(name)
+    return Nutrition_list[2:]
+
+
+def get_name_web(all_dicts,page):
+    url = "https://www.boohee.com/food/group/4?page={page}".format(page=page)
+    base_url = "https://www.boohee.com"
+    head={
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39",
+    }
+
+
+    get = requests.get(url=url,headers=head)
+    #==============
+    # with open('./水果.html', 'w',encoding="utf-8") as f:
+    #     f.write(get.text)
+    #==============
+    selector = BeautifulSoup(get.text,"html.parser")#指定html解析器
+
+
+    name_datas = selector.find_all("div",class_="text-box pull-left")
+
+    for name_data in name_datas:
+        dict = {}
+        data= name_data.find("a")
+        dict["name"] = data.text
+        add_web = get_web(str(data))
+        web =base_url +add_web
+        dict["web"] = web
+        all_dicts.append(dict)
+    return  all_dicts
+
+
+def get_continer(all_dicts):
+    for dict in all_dicts:
+        dict = all_dicts[0]
+        url = dict['web']
+        head = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Safari/537.36 Edg/99.0.1150.39",
+        }
+        get_fruits = requests.get(url=url, headers=head)
+        selector = BeautifulSoup(get_fruits.text, "html.parser")  # 指定html解析器   <div class="content">
+        continer = selector.find_all("span", class_="dt")  # 找到营养名字
+        value = selector.find_all("span", class_="dd")
+
+        N_name = Nutrition_name(continer)
+        N_value = Nutrition_value(value)
+        # print(N_name)
+        # print(N_value)
+        for i, name in enumerate(N_name):
+            dict[name] = float(N_value[i])
+    return all_dicts
+
+#===========main
+
+all_dicts = []
+for page in range(0,1):  #
+    all_dicts = get_name_web(all_dicts,page)  #获取到了名字和对应水果网页   (page是对应的页数)
+# print(all_dicts)
+all_dicts = get_continer(all_dicts)
+
+print(all_dicts)
+    #print(get_fruits.text)
+    #print(continer)
+
+
